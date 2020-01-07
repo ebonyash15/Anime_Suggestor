68b571e936b5d6ef386f66653bb988699d203559
Ebony Sharrock <ebonysharrock@gmail.com>
2020-01-07 03:54:36 +0000

    Develop primitive load dependencies

--- lib/Anime_Suggestor.rb
+++ lib/Anime_Suggestor.rb
@@ -1,6 +1,8 @@
-require "Anime_Suggestor/version"
+require_relative "./Anime_Suggestor/version"
 
 module Anime_Suggestor
   class Error < StandardError; end
   # Your code goes here...
 end
+
+require_relative './Anime_Suggestor/cli'
--- lib/Anime_Suggestor/cli.rb
+++ lib/Anime_Suggestor/cli.rb
@@ -1,3 +1,6 @@
-class CLI
-
+#CLI Controller file - responsible for user interraction
+class Studios::CLI
+  def call
+    puts "Anime-Planet's Top 5 Anime Studios:"
+  end
 end
