3ac9050280ed0b9b73fa249788cb9337bb16037b
Ebony Sharrock <ebonysharrock@gmail.com>
2020-01-10 01:26:36 +0000

    add Anime.all method

--- lib/Anime_Suggestor/animes.rb
+++ lib/Anime_Suggestor/animes.rb
@@ -1,9 +1,13 @@
 class Anime
+@@all = []
 attr_accessor :studio, :name, :year, :synapsis
   def initialize
     @name=name
     @studio=studio
     @year=year
     @synapsis=synapsis
+  end
+  def self.all
+    @@all
   end
 end
