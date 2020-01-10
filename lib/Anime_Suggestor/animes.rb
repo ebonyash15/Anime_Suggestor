ceb3bc8891e6aa261a6d43546944db8f3b4e2e1d
Ebony Sharrock <ebonysharrock@gmail.com>
2020-01-10 01:24:19 +0000

    clean up studio-anime relationship

--- lib/Anime_Suggestor/animes.rb
+++ lib/Anime_Suggestor/animes.rb
@@ -2,8 +2,7 @@ class Anime
 attr_accessor :studio, :name, :year, :synapsis
   def initialize
     @name=name
-    @studio=name
-    @studio.animes<<self
+    @studio=studio
     @year=year
     @synapsis=synapsis
   end
--- lib/Anime_Suggestor/studios.rb
+++ lib/Anime_Suggestor/studios.rb
@@ -5,23 +5,8 @@ class AnimeSuggestor::Studios
     studio_1 = self.new
     studio_1.name = "Scrape name"
     studio_1.url = "Scrape url"
-    studio_1.animes = ["Scrape anime 1",
-      "Scrape anime 2",
-      "Scrape anime 3",
-      "Scrape anime 4",
-      "Scrape anime 5",
-      "Scrape anime 6",
-      "Scrape anime 7",
-      "Scrape anime 8",
-      "Scrape anime 9",
-      "Scrape anime 10"]
-    studio_1.animes[0]={name:"Scrape anime 1 name",
-      year:"Scrape anime 1 release date",
-      synapsis: "Scrape anime 1 synapsis"}
-    studio_1.animes[1]={name:"Scrape anime 2 name",
-      year:"Scrape anime 2 release date",
-      synapsis: "Scrape anime 2 synapsis"}
-      #so on so forth
+    studio_1.animes = Anime.all.collect{|anime| anime.studio==self}
+
     studio_2 =  self.new
     studio_3 =  self.new
     studio_4 =  self.new
