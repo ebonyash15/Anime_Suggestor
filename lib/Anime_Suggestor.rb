9559a166fae8253bfa1b3f99fb25a421ac8352dd
Ebony Sharrock <ebonysharrock@gmail.com>
2020-01-10 01:30:39 +0000

    update file dependencies

--- lib/Anime_Suggestor.rb
+++ lib/Anime_Suggestor.rb
@@ -2,3 +2,4 @@
 require_relative './Anime_Suggestor/version.rb'
 require_relative './Anime_Suggestor/cli.rb'
 require_relative './Anime_Suggestor/studios.rb'
+require-relative './Anime_Suggestor/animes.rb'
