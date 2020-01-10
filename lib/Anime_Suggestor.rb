7ea8d1abd99c577e47e0fa2d466e2fa2a6fef998
Ebony Sharrock <ebonysharrock@gmail.com>
2020-01-10 01:35:45 +0000

    Add Nokogiri and Open-URI, stub scraped data

--- lib/Anime_Suggestor.rb
+++ lib/Anime_Suggestor.rb
@@ -3,3 +3,10 @@ require_relative './Anime_Suggestor/version.rb'
 require_relative './Anime_Suggestor/cli.rb'
 require_relative './Anime_Suggestor/studios.rb'
 require-relative './Anime_Suggestor/animes.rb'
+#scraped data to utilize in studios.rb and animes.rb
+require 'nokogiri'
+require 'open-uri'
+webpage=open('')
+xml=Nokogiri::HTML(webpage)
+#studio = xml.css('STUFF')
+#anime = xml.css('STUFF')
