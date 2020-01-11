8c585e65a1098e04f5145fbe7fcfd4f49afcc6fb
Ebony Sharrock <ebonysharrock@gmail.com>
2020-01-11 19:03:07 +0000

    Reorder concerns.rb

--- lib/Anime_Suggestor/concerns.rb
+++ lib/Anime_Suggestor/concerns.rb
@@ -3,18 +3,15 @@ require 'open-uri'
 module Scrape
   module InstanceMethods
     def something
-      url = 'https://www.anime-planet.com/anime/studios/'
-      webpage=open(url)
-      xml=Nokogiri::HTML(webpage)
-      studio = xml.css('a.href')
-      anime = xml.css('STUFF')
     end
   end
   module ClassMethods
     def nothing
-      url = 'https://www.anime-planet.com/anime/studios/'
-      webpage=open(url)
-      xml=Nokogiri::HTML(webpage)
     end
   end
 end
+url = 'https://www.anime-planet.com/anime/studios/'
+webpage=open(url)
+xml=Nokogiri::HTML(webpage)
+studio = xml.css('a.href')
+anime = xml.css('STUFF')
