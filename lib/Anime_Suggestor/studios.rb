6001c4f961cca00c35f4fe0bf9aab4b3e81baf3e
Ebony Sharrock <ebonysharrock@gmail.com>
2020-01-15 02:41:37 +0000

    simplify variables

--- lib/Anime_Suggestor/studios.rb
+++ lib/Anime_Suggestor/studios.rb
@@ -15,7 +15,7 @@ class AnimeSuggestor::Studios
   end
   def url
     @formatted = self.name.split(' ').join('_').scan(/\w+/).join('').split('_').join('-').downcase
-    @page = "https://www.anime-planet.com/anime/studios/#{@formatted}"
+    @url = "https://www.anime-planet.com/anime/studios/#{@formatted}"
   end
   def animes
     Anime.all.collect{|anime| anime.studio==self}
