d31e6e25beefd65057e83229d7dd69756a252af1
Ebony Sharrock <ebonysharrock@gmail.com>
2020-01-19 04:35:15 +0000

    utilize animes.rb for scraping

--- lib/Anime_Suggestor/animes.rb
+++ lib/Anime_Suggestor/animes.rb
@@ -1,43 +1,18 @@
-require_relative './concerns.rb'
-class Anime
-  @@all=[]
-  attr_reader :studio, :name, :url
-  def initialize(name, url)
-    @name=name
-    @url=url
-    @studio= Studios.all.find{|studio|studio.url==url}
-    save
-  end
-  def self.all
-    @@all
-  end
-  def save
-    @@all<<self
+require 'open-uri'
+require 'nokogiri'
+class Scraper
+  def self.scrape_studios(index_url="https://www.anime-planet.com/anime/studios/?sort=num_likes&order=desc")
+    page = Nokogiri::HTML(open(index_url))
+    studios=[]
+    page.css("tr").each do |studio|
+      name = studio.css("td.volsChRating h2 a").text
+      anime_count = studio.css("td.volsChRating a.items").text
+      anime = studio.css("td.cardHand a ul li.card").attribute("title").value
+      studio_info = {:name => name,
+        :anime_count => anime_count,
+        :recommended_anime => anime}
+      studios << studio_info
+      end
+   studios
   end
 end
-
-s1a1= Anime.new('SCRAPED NAME', url2)
-s1a2= Anime.new('SCRAPED NAME', url2)
-s1a3= Anime.new('SCRAPED NAME', url2)
-s1a4= Anime.new('SCRAPED NAME', url2)
-s1a5= Anime.new('SCRAPED NAME', url2)
-s2a1= Anime.new('SCRAPED NAME', url3)
-s2a2= Anime.new('SCRAPED NAME', url3)
-s2a3= Anime.new('SCRAPED NAME', url3)
-s2a4= Anime.new('SCRAPED NAME', url3)
-s2a5= Anime.new('SCRAPED NAME', url3)
-s3a1= Anime.new('SCRAPED NAME', url4)
-s3a2= Anime.new('SCRAPED NAME', url4)
-s3a3= Anime.new('SCRAPED NAME', url4)
-s3a4= Anime.new('SCRAPED NAME', url4)
-s3a5= Anime.new('SCRAPED NAME', url4)
-s4a1= Anime.new('SCRAPED NAME', url5)
-s4a2= Anime.new('SCRAPED NAME', url5)
-s4a3= Anime.new('SCRAPED NAME', url5)
-s4a4= Anime.new('SCRAPED NAME', url5)
-s4a5= Anime.new('SCRAPED NAME', url5)
-s5a1= Anime.new('SCRAPED NAME', url6)
-s5a2= Anime.new('SCRAPED NAME', url6)
-s5a3= Anime.new('SCRAPED NAME', url6)
-s5a4= Anime.new('SCRAPED NAME', url6)
-s5a5= Anime.new('SCRAPED NAME', url6)
